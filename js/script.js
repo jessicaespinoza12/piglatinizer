e608163b583405878e22b5b0f3806938d9c882f1
jessicaespinoza12 <jessica.espinoza@aspirestudent.org>
2019-10-24 01:24:50 +0000

    add value variable

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $( "document" ).ready(function() {
-
+console.log("ready");
     $("#button").click(function(){
-        $(".output").append("text");
+        var value = $(".input").val();
+          $(".output").append(value);
+        console.log(value);
+        alert(value);
     });
 
 });
